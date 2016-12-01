91544283d69bc2d4047aa98ddf4abbd3eb1c113f
Paolo Davila <pdavila@iesebre.com>
2016-12-01 21:02:26 +0100

    Added routes to api and web

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['middleware' => 'auth'], function() {
+    Route::resource('sutdies', 'StudiesController');
+});
 
 Route::get('/test', function () {
     $study = new Study();
