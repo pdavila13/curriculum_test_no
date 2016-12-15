c7f6e1ab700b095b1b4426fff90bb5b02a31798f
Paolo Davila <pdavila@iesebre.com>
2016-12-15 20:09:42 +0100

    All tests passed OK

--- tests/StudiesControllerTest.php
+++ tests/StudiesControllerTest.php
@@ -3,23 +3,66 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Input;
+use Scool\Curriculum\Models\Study;
+use Scool\Curriculum\Repositories\StudyRepository;
 
 class StudiesControllerTest extends TestCase
 {
     use DatabaseMigrations;
 
-//    public function testIndexNotLogin()
-//    {
-//        $this->get('studies');
-//        $this->assertRedirectedTo('login');
-//    }
+    protected $repository;
+
+    //executar el login abans d'executar els testos
+    /**
+     * StudiesControllerTest constructor.
+     */
+    public function __construct()
+    {
+        //substituir objectes per obj de mentira que definirem natros
+        $this->repository = Mockery::mock(StudyRepository::class);
+        //$this->login();
+    }
+
+    //Executar al final dels testos
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function testIndexNotLogged()
+    {
+        $this->get('studies');
+        $this->assertRedirectedTo('login');
+    }
+
+    private function createDummyStudies()
+    {
+        $study1 = new Study();
+        $study2 = new Study();
+        $study3 = new Study();
+
+        $studies = [
+            $study1,
+            $study2,
+            $study3
+        ];
+
+        return collect($studies);
+    }
 
     public function testIndex()
     {
-        //dd(route('studies.index'));
-        //$studies = factory(Scool\Curriculum\Models\Study::class,50)->create();
-        $user = factory(App\User::class)->create();
-        $this->actingAs($user);
+        //Fase 1: Preparation --> isolation/mocking
+        $this->login();
+
+        //rebras le metode all una vega i jo te dic que has de retornar "per ex: una colección"
+        $this->repository->shouldReceive('all')->once()->andReturn($this->createDummyStudies());
+        $this->repository->shouldReceive('pushCriteria')->once();
+
+        $this->app->instance(StudyRepository::class, $this->repository);
+
+
         /*
          * Aquest dos mètodes son correctes per depurar
          *
@@ -27,17 +70,29 @@ class StudiesControllerTest extends TestCase
          * $this->get('studies')->dump();
          *
          */
+
         $this->get('studies');
         $this->assertResponseOk();
         $this->assertViewHas('studies');         //Comprova una vista estatica
 
         $studies = $this->response->getOriginalContent()->getData()['studies'];
-        $this->assertInstanceOf(Illuminate\Database\Eloquent\Collection::class,$studies);
 
-        /*
-         * 1) Preparation
-         * 2) Execution
-         * 3) Assertion
-         */
+        $this->assertInstanceOf(Illuminate\Support\Collection::class,$studies);
+        $this->assertEquals(count($studies),3);
+    }
+
+    public function testStore()
+    {
+        $this->login();
+        $this->post('studies');
+
+//        $this->post('studies')->dump();
+//        $this->assertRedirectedToRoute('studies.create');        //comprovar si ha un redireccionament a una ruta
+    }
+
+    public function login()
+    {
+        $user = factory(App\User::class)->create();
+        $this->actingAs($user);
     }
 }
